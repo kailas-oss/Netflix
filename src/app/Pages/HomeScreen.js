4dbdbdcb2c08c4c37137bbd69bcf2cc499b7e3a6
Kailas M <kkailas113@gmail.com>
2024-02-03 09:52:13 +0800

    file added

---
+++ src/app/Pages/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./HomeScreen.css"
+import { Nav } from '../../components/Nav/Nav'
+export const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+
+      <Nav/>
+
+    </div>
+  )
+}
+
+export default HomeScreen;
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import "./Nav.css"
+export const Nav = () => {
+  const [show, setShow] =useState(false);
+ const transitionNavbar =() =>{
+  if(window.scrollY > 100) {
+    setShow(true);
+  }else{
+    setShow(false);
+  }
+ }
+useEffect(() => {
+  window.addEventListener("scroll",transitionNavbar);
+
+ return () => {
+ window.removeEventListener("scroll",transitionNavbar);
+}
+}, []);
+
+console.log(show);
+return (
+  <nav className={'nav ${show && "nav_black"}'}>
+      <div className='nav_contents'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png"alt=''className='nav_logo'
+    />
+    <img src='logo12.png' className="nav_avatar"/>
+       </div>
+    </nav>
+  )
+}
+export default Nav;
