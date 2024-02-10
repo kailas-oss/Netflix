e45eceec63097491cf0144d356b545aae5ffa2f3
steffindhas177@gmail.com <146445015+steffindhas@users.noreply.github.com>
2024-02-10 18:57:47 +0530

    addded

--- src/app/Pages/SignInScreen/SignInScreen.js
+++ src/app/Pages/SignInScreen/SignInScreen.js
@@ -2,7 +2,17 @@ import React from 'react'
 import "./SignInScreen.css"
 const SignInScreen = () => {
   return (
-    <div>SignInScreen</div>
+    <div className='signinScreen'>
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder='Email' type='email'/>
+        <input placeholder='Password' type ='passward'/>
+        <button type='submit'>Sign In</button>
+        <h4>
+          <span className='signinScreen_gray'>New to Netflix?</span>
+        <span className='signinScreen_link'>Sign UP</span>
+        </h4>
+        </form></div>
   )
 }
 
